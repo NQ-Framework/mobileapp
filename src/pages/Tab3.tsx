ddb2e27e48cd344dc7fc7fac1965859c53c4b387
Milos Spasojevic <milos.s.pfc@gmail.com>
2022-04-04 11:48:28 +0200

    add options for sharing

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,11 +1,72 @@
-import { IonButton, IonCard, IonCardContent, IonCardHeader, IonContent, IonHeader, IonIcon, IonLabel, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonAlert, IonButton, IonCard, IonCardContent, IonCardHeader, IonContent, IonHeader, IonIcon, IonItem, IonLabel, IonNote, IonPage, IonTitle, IonToolbar, useIonActionSheet } from '@ionic/react';
 import { cloudUploadOutline } from 'ionicons/icons';
+import { useState } from 'react';
 import { useList } from '../hooks/use-list.hook';
 import { arrayToCsv } from '../utilities/array-to-csv';
 import './Tab3.css';
 
 const Tab3: React.FC = () => {
   const { list, count } = useList();
+  const [infoDialogOpen, setInfoDialogOpen] = useState<boolean>(false);
+
+  const [presentActionSheet] = useIonActionSheet();
+
+
+  const handleSendButtonClick = () => {
+
+    if (count === 0) {
+      setInfoDialogOpen(true);
+      return;
+    }
+    const csvString = arrayToCsv(list.map(l => [l.value, l.timestamp, l.id]));
+    const fileName = `${lastDate.toLocaleString()} - ${count} - ${firstDate.toLocaleString()}.csv`;
+    const urlEncodeFileName = encodeURIComponent(fileName);
+
+
+    presentActionSheet({
+      header: 'Na koji način želite da pošaljete listu?',
+      buttons: [
+        {
+          text: 'Share',
+          handler: () => {
+            navigator.share({
+              files: [
+                new File([new Blob([csvString], { type: 'text/csv' })], fileName)
+              ]
+            });
+          },
+        },
+        {
+          text: 'Download CSV',
+          handler: () => {
+            const a = document.createElement('a');
+            a.href = `data:text/csv;charset=utf-8,${csvString}`;
+            a.download = fileName;
+            a.click();
+          }
+        },
+        {
+          text: 'Clipboard',
+          handler: () => {
+            navigator.clipboard.writeText(csvString);
+          }
+        },
+        {
+          text: 'Email',
+          handler: () => {
+            window.open(`mailto:?subject=Popis%20Lista%20${urlEncodeFileName}&body=${csvString}`);
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        }
+      ],
+    })
+
+
+  };
+
 
   let firstDate = new Date();
   let lastDate = new Date(0);
@@ -28,13 +89,14 @@ const Tab3: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
+        <IonAlert isOpen={infoDialogOpen}
+          onDidDismiss={() => { setInfoDialogOpen(false); }}
+          header='Nema skeniranih stavki'
+          message='Da bi mogli da pošaljete listu, morate da skenirate bar jednu stavku.'
+        />
         <div className='container'>
           <IonButton fill='outline' className='upload-button' onClick={() => {
-            navigator.share({
-              files: [
-                new File([new Blob([arrayToCsv(list.map(l => [l.value, l.timestamp, l.id]))], { type: 'text/csv' })], 'list.csv')
-              ]
-            });
+            handleSendButtonClick();
           }} size='small'>
             <IonIcon className='upload-icon' icon={cloudUploadOutline}></IonIcon>
           </IonButton>
@@ -44,15 +106,31 @@ const Tab3: React.FC = () => {
             </IonCardHeader>
             <IonCardContent>
               <div className='stat-container'>
-                <IonLabel>
-                  Broj skeniranih stavki: {count}
-                </IonLabel>
-                {/* <IonLabel>
-                  Datum prve stavke: {firstDate.toTimeString()}
-                </IonLabel>
-                <IonLabel>
-                  Datum poslednje stavke: {lastDate.toTimeString()}
-                </IonLabel> */}
+                <IonItem>
+                  <IonLabel>
+                    Broj stavki
+                  </IonLabel>
+                  <IonNote slot='start'>
+                    {count}
+                  </IonNote>
+                </IonItem>
+                {count > 0 && <><IonItem>
+                  <IonLabel>
+                    Vreme prve stavke
+                  </IonLabel>
+                  <IonNote slot='start'>
+                    {firstDate.toLocaleTimeString()}
+                  </IonNote>
+                </IonItem>
+                  <IonItem>
+                    <IonLabel>
+                      Vreme poslednje stavke
+                    </IonLabel>
+                    <IonNote slot='start'>
+                      {lastDate.toLocaleTimeString()}
+                    </IonNote>
+                  </IonItem>
+                </>}
               </div>
             </IonCardContent>
           </IonCard>
